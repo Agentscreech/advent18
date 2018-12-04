1672cc26591afb4a459e2a23a3083c13f6675612
agentscreech <travis.walentin@gmail.com>
2018-12-04 00:04:02 -0800

    day2 both stars

---
+++ 02.1/day2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("day2.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	twosCounter := 0
+	threesCounter := 0
+	for scanner.Scan() {
+		var twos bool
+		var threes bool
+		letters := map[rune]int{}
+		for _, letter := range scanner.Text() {
+			if _, found := letters[letter]; !found {
+				letters[letter] = 1
+			} else {
+				letters[letter]++
+			}
+		}
+		for i := range letters {
+			if letters[i] == 2 && !twos {
+				twos = true
+				twosCounter++
+			}
+			if letters[i] == 3 && !threes {
+				threes = true
+				threesCounter++
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(twosCounter * threesCounter)
+
+}
