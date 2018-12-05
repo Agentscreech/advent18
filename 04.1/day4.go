ca6a15d6d084aaa5a02203ff4025b55fa05d5e5f
agentscreech <travis.walentin@gmail.com>
2018-12-05 14:54:37 -0800

    added instructions files for prior days and scaffolled day 4

---
+++ 04.1/day4.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	//open file
+
+	//parse data, first set is date/time, second is either Guard start or sleep/wake event
+
+	//sort times
+
+	//find which guard slept the most
+
+	//find which minute that guard slept the most
+
+	//return guard id * most min slept.
+
+}
